c89c8e2c42f09f5eac2325189705d9e9265c3563
Carver <pcarverb@hotmail.com>
2017-04-23 00:15:35 -0700

    ARCHIVESSS!

---
+++ komputerkid/index.md
@@ -0,0 +1,4 @@
+# Welcome to the KomputerKid.net Archive!
+[Go Back](..)
+## Websites
+* [backup.komputerkid.net](websites/backup.computerkid.net)
---
+++ raytron/fourms/talk.raytron.org/syndication.php
@@ -0,0 +1,214 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/">
+	<channel>
+		<title><![CDATA[Raytron - All Forums]]></title>
+		<link>https://talk.raytron.org/</link>
+		<description><![CDATA[Raytron - https://talk.raytron.org]]></description>
+		<pubDate>Sun, 23 Apr 2017 06:33:57 +0000</pubDate>
+		<generator>MyBB</generator>
+		<item>
+			<title><![CDATA[Requests to moderators]]></title>
+			<link>https://talk.raytron.org/Thread-Pinned-Requests-to-moderators</link>
+			<pubDate>Thu, 16 Mar 2017 21:55:12 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Pinned-Requests-to-moderators</guid>
+			<description><![CDATA[In this topic you can request thread unlocks, thread ownership transfers, thread clean-ups. For bad posts/topics, please use the report system. If you want to report a moderator, send a private message to V0rTeX. Do not fight!]]></description>
+			<content:encoded><![CDATA[In this topic you can request thread unlocks, thread ownership transfers, thread clean-ups. For bad posts/topics, please use the report system. If you want to report a moderator, send a private message to V0rTeX. Do not fight!]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Changelog]]></title>
+			<link>https://talk.raytron.org/Thread-Pinned-Changelog</link>
+			<pubDate>Thu, 09 Mar 2017 07:33:50 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Pinned-Changelog</guid>
+			<description><![CDATA[Official changelog topic.]]></description>
+			<content:encoded><![CDATA[Official changelog topic.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[techTalk Archive is back!]]></title>
+			<link>https://talk.raytron.org/Thread-Announcement-techTalk-Archive-is-back</link>
+			<pubDate>Thu, 19 Jan 2017 11:50:15 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Announcement-techTalk-Archive-is-back</guid>
+			<description><![CDATA[The techTalk archive is finally back online.Â <br />
+<a href="https://archive.raytron.org/techtalk/" target="_blank" class="mycode_url">https://archive.raytron.org/techtalk/</a>]]></description>
+			<content:encoded><![CDATA[The techTalk archive is finally back online.Â <br />
+<a href="https://archive.raytron.org/techtalk/" target="_blank" class="mycode_url">https://archive.raytron.org/techtalk/</a>]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[What did you get/do for Christmas?]]></title>
+			<link>https://talk.raytron.org/Thread-Lounge-General-What-did-you-get-do-for-Christmas</link>
+			<pubDate>Thu, 29 Dec 2016 07:50:30 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Lounge-General-What-did-you-get-do-for-Christmas</guid>
+			<description><![CDATA[Just curious as to what everyone got/did for Christmas. I'll start:<br />
+<br />
+For "Christmas" (the one we have about 10 days before actual Christmas with my grandparents) I got a bluetooth speaker, a pair of jeans, and a green shirt.<br />
+<br />
+For actual Christmas, I got a bunch of clothes, a Raspberry Pi 3 B kit, a phone dock, a set of 7 colorful socks, another pair of socks, a bag full of chocolate, a pair of gloves, a tech magazine, chocolates, a thing of shampoo + body wash, a Raspberry Pi 3 Guide, and I also bought myself a few late Christmas presents, including an iPad dock, Core 2 Quad Q6700, 4 GB DDR 2 RAM, a MicroATX board, an R7 350, and some thermal paste.<br />
+<br />
+For my Birthday (which is 9 days before Christmas) I got a pair of athletic shorts and an athletic shirt and athletic shoes, reflective stickers for my helmet and shoes, a spare rear light for my bike, and &#36;140.<br />
+<br />
+I spent most of Christmas at my grandparents cabin, sleeping, because I had worked for 6 days in a row prior to Christmas and I was exhausted. I missed Christmas dinner unfortunately. <img src="https://talk.raytron.org/images/smilies/sad.png" alt="Sad" title="Sad" class="smilie smilie_8" />]]></description>
+			<content:encoded><![CDATA[Just curious as to what everyone got/did for Christmas. I'll start:<br />
+<br />
+For "Christmas" (the one we have about 10 days before actual Christmas with my grandparents) I got a bluetooth speaker, a pair of jeans, and a green shirt.<br />
+<br />
+For actual Christmas, I got a bunch of clothes, a Raspberry Pi 3 B kit, a phone dock, a set of 7 colorful socks, another pair of socks, a bag full of chocolate, a pair of gloves, a tech magazine, chocolates, a thing of shampoo + body wash, a Raspberry Pi 3 Guide, and I also bought myself a few late Christmas presents, including an iPad dock, Core 2 Quad Q6700, 4 GB DDR 2 RAM, a MicroATX board, an R7 350, and some thermal paste.<br />
+<br />
+For my Birthday (which is 9 days before Christmas) I got a pair of athletic shorts and an athletic shirt and athletic shoes, reflective stickers for my helmet and shoes, a spare rear light for my bike, and &#36;140.<br />
+<br />
+I spent most of Christmas at my grandparents cabin, sleeping, because I had worked for 6 days in a row prior to Christmas and I was exhausted. I missed Christmas dinner unfortunately. <img src="https://talk.raytron.org/images/smilies/sad.png" alt="Sad" title="Sad" class="smilie smilie_8" />]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Terror attack unfolds in Berlin at a Christmas market]]></title>
+			<link>https://talk.raytron.org/Thread-Terror-attack-unfolds-in-Berlin-at-a-Christmas-market</link>
+			<pubDate>Tue, 20 Dec 2016 07:52:09 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Terror-attack-unfolds-in-Berlin-at-a-Christmas-market</guid>
+			<description><![CDATA[<a href="http://www.mirror.co.uk/news/world-news/berlin-terror-attack-live-updates-9486959" target="_blank" class="mycode_url">http://www.mirror.co.uk/news/world-news/...es-9486959</a><br />
+<br />
+Sent from my Nexus 5X using Tapatalk]]></description>
+			<content:encoded><![CDATA[<a href="http://www.mirror.co.uk/news/world-news/berlin-terror-attack-live-updates-9486959" target="_blank" class="mycode_url">http://www.mirror.co.uk/news/world-news/...es-9486959</a><br />
+<br />
+Sent from my Nexus 5X using Tapatalk]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Username change and custom rank requests]]></title>
+			<link>https://talk.raytron.org/Thread-Pinned-Username-change-and-custom-rank-requests</link>
+			<pubDate>Sun, 18 Dec 2016 03:59:30 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Pinned-Username-change-and-custom-rank-requests</guid>
+			<description><![CDATA[In this topic you can a username change, or request a custom rank <span style="font-weight: bold;" class="mycode_b">once per 6 months</span>.<br />
+<br />
+Requirements:<br />
+* Registered for 3+ months<br />
+* At least 100 posts]]></description>
+			<content:encoded><![CDATA[In this topic you can a username change, or request a custom rank <span style="font-weight: bold;" class="mycode_b">once per 6 months</span>.<br />
+<br />
+Requirements:<br />
+* Registered for 3+ months<br />
+* At least 100 posts]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Happy Birthday, KomputerKid!]]></title>
+			<link>https://talk.raytron.org/Thread-Lounge-General-Happy-Birthday-KomputerKid</link>
+			<pubDate>Fri, 16 Dec 2016 12:17:02 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Lounge-General-Happy-Birthday-KomputerKid</guid>
+			<description><![CDATA[Our staff member - KomputerKid turns 20 today.]]></description>
+			<content:encoded><![CDATA[Our staff member - KomputerKid turns 20 today.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[What.cd Shuts Down Following Reported Raids in France]]></title>
+			<link>https://talk.raytron.org/Thread-Lounge-General-What-cd-Shuts-Down-Following-Reported-Raids-in-France</link>
+			<pubDate>Fri, 18 Nov 2016 01:36:38 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Lounge-General-What-cd-Shuts-Down-Following-Reported-Raids-in-France</guid>
+			<description><![CDATA[<a href="https://torrentfreak.com/what-cd-shuts-down-following-reported-raids-in-france-161117/" target="_blank" class="mycode_url">https://torrentfreak.com/what-cd-shuts-d...ce-161117/</a>]]></description>
+			<content:encoded><![CDATA[<a href="https://torrentfreak.com/what-cd-shuts-down-following-reported-raids-in-france-161117/" target="_blank" class="mycode_url">https://torrentfreak.com/what-cd-shuts-d...ce-161117/</a>]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Who you supported for POTUS]]></title>
+			<link>https://talk.raytron.org/Thread-Lounge-General-Who-you-supported-for-POTUS</link>
+			<pubDate>Thu, 10 Nov 2016 04:59:05 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Lounge-General-Who-you-supported-for-POTUS</guid>
+			<description><![CDATA[Since Donald Trump has won, I am curious to see who everyone truly supported for POTUS.<br />
+<br />
+I am not a Trump supporter personally. I just voted for him more out of a "Hillary needs to be in jail, and voting 3rd party is a waste of time" standpoint. The person I would have actually liked to see win, is Jill Stein, but of course, I know that would probably never have happened, and probably never will happen.]]></description>
+			<content:encoded><![CDATA[Since Donald Trump has won, I am curious to see who everyone truly supported for POTUS.<br />
+<br />
+I am not a Trump supporter personally. I just voted for him more out of a "Hillary needs to be in jail, and voting 3rd party is a waste of time" standpoint. The person I would have actually liked to see win, is Jill Stein, but of course, I know that would probably never have happened, and probably never will happen.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Donald Trump is the new president of United States]]></title>
+			<link>https://talk.raytron.org/Thread-Politics-Donald-Trump-is-the-new-president-of-United-States</link>
+			<pubDate>Wed, 09 Nov 2016 17:01:51 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Politics-Donald-Trump-is-the-new-president-of-United-States</guid>
+			<description><![CDATA[<div align="center"><img src="http://i.imgur.com/2yHT531.png" alt="[Image: 2yHT531.png]" class="mycode_img" /><br />
+<br />
+<br />
+<iframe width="560" height="315" src="//www.youtube.com/embed/owuq_An4cnk" frameborder="0" allowfullscreen=""></iframe></div>
+<br />
+Discuss.]]></description>
+			<content:encoded><![CDATA[<div align="center"><img src="http://i.imgur.com/2yHT531.png" alt="[Image: 2yHT531.png]" class="mycode_img" /><br />
+<br />
+<br />
+<iframe width="560" height="315" src="//www.youtube.com/embed/owuq_An4cnk" frameborder="0" allowfullscreen=""></iframe></div>
+<br />
+Discuss.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[New BB codes]]></title>
+			<link>https://talk.raytron.org/Thread-Announcement-New-BB-codes</link>
+			<pubDate>Wed, 09 Nov 2016 04:40:16 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Announcement-New-BB-codes</guid>
+			<description><![CDATA[Hello. I added some new bb codes - [youtube], [facebook], [instagram], [twitter] and [spotify]. You can now easily embed content from these platforms. Enjoy.]]></description>
+			<content:encoded><![CDATA[Hello. I added some new bb codes - [youtube], [facebook], [instagram], [twitter] and [spotify]. You can now easily embed content from these platforms. Enjoy.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Raytron available on Tapatalk network again]]></title>
+			<link>https://talk.raytron.org/Thread-Announcement-Raytron-available-on-Tapatalk-network-again</link>
+			<pubDate>Fri, 04 Nov 2016 03:25:46 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Announcement-Raytron-available-on-Tapatalk-network-again</guid>
+			<description><![CDATA[After nearly a year of absence, Raytron isÂ available on Tapatalk network again. For these who don't know what Tapatalk is - basicially it's app which simplifies using forums on mobile devices.]]></description>
+			<content:encoded><![CDATA[After nearly a year of absence, Raytron isÂ available on Tapatalk network again. For these who don't know what Tapatalk is - basicially it's app which simplifies using forums on mobile devices.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Happy Halloween!]]></title>
+			<link>https://talk.raytron.org/Thread-Lounge-General-Happy-Halloween</link>
+			<pubDate>Tue, 01 Nov 2016 00:37:01 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Lounge-General-Happy-Halloween</guid>
+			<description><![CDATA[Happy Halloween to everyone who celebrates it!]]></description>
+			<content:encoded><![CDATA[Happy Halloween to everyone who celebrates it!]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[New staff additions - KomputerKid and esc1010]]></title>
+			<link>https://talk.raytron.org/Thread-Announcement-New-staff-additions-KomputerKid-and-esc1010</link>
+			<pubDate>Mon, 31 Oct 2016 01:33:59 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Announcement-New-staff-additions-KomputerKid-and-esc1010</guid>
+			<description><![CDATA[KomputerKid, a previous co-owner/administrator and esc1010 have been promoted to Global Moderators. I'm wishing them best luck with their new duties.]]></description>
+			<content:encoded><![CDATA[KomputerKid, a previous co-owner/administrator and esc1010 have been promoted to Global Moderators. I'm wishing them best luck with their new duties.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Surface Studio/Macbook Pro thoughts?]]></title>
+			<link>https://talk.raytron.org/Thread-Technology-General-Surface-Studio-Macbook-Pro-thoughts</link>
+			<pubDate>Sat, 29 Oct 2016 23:47:26 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Technology-General-Surface-Studio-Macbook-Pro-thoughts</guid>
+			<description><![CDATA[Just trying to open up another topic for discussion. What are your thoughts on the new Microsoft and Apple announcements this week?]]></description>
+			<content:encoded><![CDATA[Just trying to open up another topic for discussion. What are your thoughts on the new Microsoft and Apple announcements this week?]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Last movie you watched?]]></title>
+			<link>https://talk.raytron.org/Thread-Movies-TV-Last-movie-you-watched</link>
+			<pubDate>Sat, 29 Oct 2016 19:34:11 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Movies-TV-Last-movie-you-watched</guid>
+			<description><![CDATA[What was the last movie you watched?]]></description>
+			<content:encoded><![CDATA[What was the last movie you watched?]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Your favorite YouTube channels?]]></title>
+			<link>https://talk.raytron.org/Thread-Lounge-General-Your-favorite-YouTube-channels</link>
+			<pubDate>Sat, 29 Oct 2016 03:17:14 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Lounge-General-Your-favorite-YouTube-channels</guid>
+			<description><![CDATA[Share your favorite YouTube channels.]]></description>
+			<content:encoded><![CDATA[Share your favorite YouTube channels.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Games you're currently playing?]]></title>
+			<link>https://talk.raytron.org/Thread-Gaming-General-Games-you-re-currently-playing</link>
+			<pubDate>Sat, 29 Oct 2016 02:22:42 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Gaming-General-Games-you-re-currently-playing</guid>
+			<description><![CDATA[Share which games you're currently playing.]]></description>
+			<content:encoded><![CDATA[Share which games you're currently playing.]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[Your favorite gaming platform?]]></title>
+			<link>https://talk.raytron.org/Thread-Gaming-General-Your-favorite-gaming-platform</link>
+			<pubDate>Sat, 29 Oct 2016 02:16:10 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Gaming-General-Your-favorite-gaming-platform</guid>
+			<description><![CDATA[What is your favorite gaming platform?]]></description>
+			<content:encoded><![CDATA[What is your favorite gaming platform?]]></content:encoded>
+		</item>
+		<item>
+			<title><![CDATA[What mobile phone do you currently use?]]></title>
+			<link>https://talk.raytron.org/Thread-Mobile-What-mobile-phone-do-you-currently-use</link>
+			<pubDate>Sat, 29 Oct 2016 02:07:27 +0000</pubDate>
+			<guid isPermaLink="false">https://talk.raytron.org/Thread-Mobile-What-mobile-phone-do-you-currently-use</guid>
+			<description><![CDATA[Share what mobile phone do you currently use.]]></description>
+			<content:encoded><![CDATA[Share what mobile phone do you currently use.]]></content:encoded>
+		</item>
+	</channel>
+</rss>
